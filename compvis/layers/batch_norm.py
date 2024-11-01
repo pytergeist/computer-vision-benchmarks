4d61f5aa665066560a4de682ed367bd167030892
pytergeist <tompopeworks@gmail.com>
2024-11-01 18:21:39 +0000

    Adding batch norm skeleton

--- compvis/layers/batch_norm.py
+++ compvis/layers/batch_norm.py
@@ -1,1 +1,19 @@
 # batch_norm.py
+
+# conv_2d.py
+
+import tensorflow as tf
+
+
+class BatchNormalisation(tf.keras.layers.Layer):
+    def __init__(self):
+        pass
+
+    def build(self, input_shape):
+        pass
+
+    def call(self, inputs, **kwargs):
+        pass
+
+    def compute_output_shape(self, input_shape):
+        pass
